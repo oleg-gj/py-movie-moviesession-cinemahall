44852fb76a7a13ea63cb9766f9fdf5f12b10daef
Oleh <musiychuk09@gmail.com>
2024-07-02 14:45:11 -0400

    was added "related_name" for ForeignKey

--- services/movie_session.py
+++ services/movie_session.py
@@ -19,7 +19,7 @@ def create_movie_session(
 
 def get_movies_sessions(
         session_date: str = None
-) -> QuerySet[MovieSession] | MovieSession:
+) -> QuerySet[MovieSession]:
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
     return MovieSession.objects.all()
